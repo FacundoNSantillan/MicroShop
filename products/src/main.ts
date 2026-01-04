91f3844e6223857bc4f736bc6f1b63cd74ed77e5
Facundo Santillan <santillanfacundo43@gmail.com>
2026-01-03 21:00:33 -0300

    feat(products): setup Prisma + PostgreSQL with Docker and initial migration

--- products/src/main.ts
+++ products/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3002);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('MicroShop Products API')
+    .setDescription('Products microservice')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('products/docs', app, document);
+
+  await app.listen(3002);
 }
 bootstrap();
---
+++ products/src/products/products.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Products')
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+}
